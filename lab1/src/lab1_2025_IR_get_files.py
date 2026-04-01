f72b605bd4fab7d8a96f30b2a1f2a11438ca2cb4
seansha30 <shamaoxuan@gmail.com>
2026-04-01 23:49:58 +0200

    extract Reuters dataset and 1111 subset

---
+++ lab1/src/lab1_2025_IR_get_files.py
@@ -0,0 +1,41 @@
+import os
+import nltk
+from nltk.corpus import reuters
+nltk.download('reuters')  # TODO: Uncomment if needed
+
+from pathlib import Path
+
+def main():
+    os.makedirs('../reuters/test', exist_ok=True)  # This creates a directory unless you already have one.
+    os.makedirs('../reuters/training', exist_ok=True)
+    # Make sure the path is correct (i.e., is in accordance with the structure of directories and files under Section 8 
+    #in the lab instructions
+    out_path = Path('../reuters')
+    small_set_path = Path('../reuters_small')
+
+    # TODO: Get the list of Reuters file IDs
+    # TODO: For each file ID:
+    #   - Get the raw text
+    #   - Create a filename based on the file ID
+    #   - Write the text to a .txt file in './reuters'
+    #   - Optionally: print the filename
+    
+    file_list = reuters.fileids()
+
+    for file in file_list:
+        raw_text = reuters.raw(file)
+        out_file = out_path / f'{file}.txt'
+        with out_file.open('w', encoding='utf-8') as f:
+            f.write(raw_text)
+        
+        _, doc_name = file.split('/', 1)
+        if doc_name.startswith('1111'):
+            out_file = small_set_path / f'{doc_name}.txt'
+            with out_file.open('w', encoding='utf-8') as f:
+                f.write(raw_text)
+
+
+
+if __name__ == '__main__':
+    main()
+
